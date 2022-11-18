d009c9df69f232ed560cd56dac5bc25168e04a7d
william <myname@example.com>
2022-11-19 02:06:54 +0800

    add udp dial, 设置rxhandler,这样可以设置是否手动调用读操作. 默认是后台起一个goroutine 负责批量读数据，应用层代码只需当时io.Reader 一个个读取报文

---
+++ pkg/udp/dial.go
@@ -0,0 +1,73 @@
+package udp
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"golang.org/x/net/ipv4"
+)
+
+func UdpDial(ctx context.Context, network, laddr, raddr string, opts ...UDPConnOpt) (net.Conn, error) {
+	return DialWithOpt(ctx, network, laddr, raddr, opts...)
+}
+
+func DialWithOpt(ctx context.Context, network, laddr, raddr string, opts ...UDPConnOpt) (*UDPConn, error) {
+	var err error
+	la := &net.UDPAddr{IP: net.IPv4zero, Port: 0}
+	if laddr != "" {
+		la, err = net.ResolveUDPAddr(network, laddr)
+		if err != nil {
+			return nil, err
+		}
+	}
+	ra, err := net.ResolveUDPAddr(network, raddr)
+	if err != nil {
+		return nil, err
+	}
+
+	lconn, err := net.DialUDP(network, la, ra)
+	if err != nil {
+		return nil, err
+	}
+
+	c := NewUDPConn(nil, lconn, ra, opts...)
+	if c.rxhandler != nil {
+		go c.ReadBatchLoop(c.rxhandler)
+	}
+	return c, nil
+}
+
+func (c *UDPConn) ReadBatchLoop(handler func(msg []byte)) error {
+	readBatchs := c.readBatchs
+	maxBufSize := c.maxBufSize
+	pc := c.pc
+
+	rms := make([]ipv4.Message, readBatchs)
+	for i := 0; i < len(rms); i++ {
+		rms[i] = ipv4.Message{Buffers: [][]byte{make([]byte, maxBufSize)}}
+	}
+	for {
+		n, err := pc.ReadBatch(rms, 0)
+		if err != nil {
+			c.Close()
+			return err
+		}
+		log.Printf("got n:%d, len(ms):%d\n", n, len(rms))
+
+		if n == 0 {
+			continue
+		}
+		for i := 0; i < n; i++ {
+			if handler != nil {
+				handler(rms[i].Buffers[0][:rms[i].N])
+			}
+		}
+	}
+}
+
+func (c *UDPConn) handlePacket(msg []byte) {
+	b := make([]byte, len(msg))
+	copy(b, msg)
+	c.PutRxQueue(b)
+}
