bc594243f23f7c60b32d1398e5a03c1a9467e1bb
william <myname@example.com>
2023-07-01 15:12:50 +0800

    print req header

--- cmd/goproxytest/server.go
+++ cmd/goproxytest/server.go
@@ -36,6 +36,6 @@ func echo(w http.ResponseWriter, req *http.Request) {
 	//正常接收get.go 请求：echo handler, method:GET, Host:127.0.0.1:1313, url:/echo
 	//接收代理tr.Proxy的请求：echo handler, method:GET, Host:127.0.0.2:1313, url:http://127.0.0.2:1313/echo
 	//CONNECT 的请求，这里HOST 可以是任意，不一定是127.0.0.1:1313, 比如打印echo handler, method:Get, Host:1.1.1.1:1234, url:/echo
-	fmt.Printf("echo handler, method:%s, Host:%v, url:%#v\n", req.Method, req.Host, req.URL)
+	fmt.Printf("echo handler, method:%s, Host:%v, url:%#v, header:%v\n", req.Method, req.Host, req.URL, req.Header)
 	w.Write([]byte("echo"))
 }
