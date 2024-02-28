3e31911a4b088967d564d886d97b88f4f6eff32c
william <myname@example.com>
2024-02-28 20:03:47 +0800

    zinx 超时流程注释

--- cmd/zinx/heartbeat/server.go
+++ cmd/zinx/heartbeat/server.go
@@ -43,6 +43,38 @@ func main() {
 		Router:           &myHeartBeatRouter{},
 		HeartBeatMsgID:   uint32(myHeartBeatMsgID),
 	})
-
+	//服务启动后， 会启动 服务的心跳机制, 定时去检测连接是否IsAlive(), 如何不是Alive(), 就调用回调onRemoteNotAlive 通知业务层
+	//，连接收到一个报文，就会更新lastActivityTime
 	s.Serve()
 }
+
+/*
+func (h *HeartbeatChecker) check() (err error) {
+
+	if h.conn == nil {
+		return nil
+	}
+ 	//莫：定时去检测连接是否存活
+	if !h.conn.IsAlive() {
+		h.onRemoteNotAlive(h.conn)
+	} else {
+		if h.beatFunc != nil {
+			err = h.beatFunc(h.conn)
+		} else {
+			err = h.SendHeartBeatMsg()
+		}
+	}
+
+	return err
+}
+//判断 检测连接是否存活，其实就是判断c.lastActivityTime 最后收到报文的时间是否超时。
+func (c *Connection) IsAlive() bool {
+	if c.isClosed {
+		return false
+	}
+	// Check the last activity time of the connection. If it's beyond the heartbeat interval,
+	// then the connection is considered dead.
+	// (检查连接最后一次活动时间，如果超过心跳间隔，则认为连接已经死亡)
+	return time.Now().Sub(c.lastActivityTime) < zconf.GlobalObject.HeartbeatMaxDuration()
+}
+*/
